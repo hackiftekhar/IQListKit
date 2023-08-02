358df6ec1b40b6f9c09442c7d41c16bd010fc8b6
hackiftekhar <hack.iftekhar@gmail.com>
2023-08-03 02:49:56 +0530

    Added IQListKit.xcodeproj for Carthage compatibility

---
+++ IQListKit/IQListKit.h
@@ -0,0 +1,18 @@
+//
+//  IQListKit.h
+//  IQListKit
+//
+//  Created by Iftekhar on 8/3/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IQListKit.
+FOUNDATION_EXPORT double IQListKitVersionNumber;
+
+//! Project version string for IQListKit.
+FOUNDATION_EXPORT const unsigned char IQListKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IQListKit/PublicHeader.h>
+
+
